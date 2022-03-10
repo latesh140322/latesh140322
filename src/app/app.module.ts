b508c836574c130fad0344d4cac2958548fc49cc
Himanshu-b-29 <himanshu.barapatre@plusneed.com>
2022-03-10 12:58:54 +0530

    dashboard changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { IncomeComponent } from './income/income.component';
 import { IncomechecklistComponent } from './incomechecklist/incomechecklist.component';
 import { IncomehistoryComponent } from './incomehistory/incomehistory.component';
 import { LeftsidemenuComponent } from './leftsidemenu/leftsidemenu.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { LeftsidemenuComponent } from './leftsidemenu/leftsidemenu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  default_Type = null;
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+
   }
 
+  onRoute(formValues : NgForm){
+    const route = formValues.value.type_DLV;
+    if(route){
+      this.route.navigate([`${route}`]);
+    }else{
+      console.log("under development");
+      
+    }
+    console.log(formValues.value);
+    
+  }
 }
