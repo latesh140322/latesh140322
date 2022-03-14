b56cb8a20ae4f7b50770ca04bc9742b8de0ff721
Himanshu-b-29 <himanshu.barapatre@plusneed.com>
2022-03-14 15:42:22 +0530

    income form validation

--- src/app/incomeform/incomeform.component.ts
+++ src/app/incomeform/incomeform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-incomeform',
@@ -16,7 +17,8 @@ export class IncomeformComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  onGenerateDLV(){
-
+  onGenerateDLV(form : NgForm){
+    console.log(form);
+    
   }
 }
