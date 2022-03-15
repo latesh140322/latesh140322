b0861da57513b709f4e86d0af74a4143a6236cfe
ratan-one <98743120+ratan-one@users.noreply.github.com>
2022-03-15 16:57:57 +0530

    Created Component Folder

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,19 +11,24 @@ import { Route, Router } from '@angular/router';
 export class DashboardComponent implements OnInit {
 
   default_Type = null;
+  error_DLV : Boolean = false
+
+ 
   constructor(private route: Router) { }
+
 
   ngOnInit(): void {
 
-  }
-
+   }
+    
   onRoute(formValues : NgForm){
+    this.error_DLV = true;
     const route = formValues.value.type_DLV;
+
     if(route){
       this.route.navigate([`${route}`]);
     }else{
       console.log("under development");
-      
     }
     console.log(formValues.value);
     
--- src/app/income-component/incomeform/incomeform.component.ts
+++ src/app/income-component/incomeform/incomeform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-incomeform',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./incomeform.component.scss']
 })
 export class IncomeformComponent implements OnInit {
-
+  default_Rec = null;
+  default_Serv = null;
+  default_loc = null;
+  default_evn = null;
+  default_pyt = null;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onGenerateDLV(form : NgForm){
+    console.log(form);
+    
+  }
 }
